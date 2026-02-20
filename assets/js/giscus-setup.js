21dd659df5527f09904b3b067772afee9ec1ef02
himsriv24 <93821317+himsriv24@users.noreply.github.com>
2026-02-20 20:00:19 +0000

    Deploying to gh-pages from @ himsriv24/himsriv24.github.io@8d8399433090e7c1f9fc6ac5fca6c8a13bad7993 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "himsriv24/himanshukumar.github.io",
+    "data-repo": "himsriv24/himsriv24.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
